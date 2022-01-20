8e8201799bfc81c35828d8edb02bb38a8392ccc0
rajoy99 <rafat.joy99@gmail.com>
2022-01-20 19:23:07 +0600

    button problems

--- part1/1.6to1.14/src/App.js
+++ part1/1.6to1.14/src/App.js
@@ -14,10 +14,17 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+
+
   return (
     <div>
       Give Feedback
-      <Button onClick={goodclick} text={Good} />
+      <br></br>
+      <Button handleClick={setGood(good + 1)} text="Good" />
+      <br></br>
+      Statistics
+      <br></br>
+      {good}
 
     </div>
   )
