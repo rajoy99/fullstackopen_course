7520bc4d627d53a1b5d424815e8344ee9d8e57cd
rajoy99 <rafat.joy99@gmail.com>
2022-01-20 19:11:15 +0600

    several

--- part1/1.6to1.14/src/App.js
+++ part1/1.6to1.14/src/App.js
@@ -1,104 +1,28 @@
-import React from 'react'
 
+import React, { useState } from 'react'
 
-
-const Header = (props) => {
-
-    return (
-        <div>
-          <h1> {props.course} </h1>
-        </div>
-    )
-
-}
-
-const Part = (props) =>{
-
-  return (
-    <div>
-      {props.part} {props.exercise}
-    </div>
-  )
-
-}
-
-
-const Content = () => {
-
-
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-
-  return(
-    <div>
-      <Part part={part1.name} exercise={part1.exercises} />
-      <Part part={part2.name} exercise={part2.exercises} />
-      <Part part={part3.name} exercise={part3.exercises} />
-    </div>
-
-  )
-
-}
-
-
-const Total = (props) => {
-
-  return (
-    <div>
-      <p>
-      Number of exercises {props.exercise}
-      </p>
-    </div>
-  )
-
-}
-
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
 
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
- var container= new Array(3)
-
-  for(let i=0;i<3;i++){
-    container[i]=<p> {course.parts[i].name} {course.parts[i].exercises} </p>
-  }
- 
   return (
     <div>
-      <h1>{course.name}</h1>
-      {container}
+      Give Feedback
+      <Button onClick={goodclick} text={Good} />
+
     </div>
   )
 }
 
-
-
 export default App
+
+
