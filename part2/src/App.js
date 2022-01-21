3e2736c87495de0a7837dcfadda7d7b5e007565b
rajoy99 <rafat.joy99@gmail.com>
2022-01-21 20:06:02 +0600

    2.4 done

--- part2/src/App.js
+++ part2/src/App.js
@@ -4,13 +4,14 @@ import React from 'react'
 
 const Course = (props) =>{
 
-  
-
   return(
    <div>
-     <h1>{props.course.name}</h1>
-      {props.course.parts.map(val=><p>{val.name} {val.exercises}</p>)}
-      total of {props.value} courses
+     <h1>{props.courses[0].name}</h1>
+      {props.courses[0].parts.map(val=><p>{val.name} {val.exercises}</p>)}
+      <h4>total of {props.courses[0].parts.reduce(function(acc,obj){return acc+obj.exercises},0)} courses</h4>
+      <h2>{props.courses[1].name}</h2>
+      {props.courses[1].parts.map(val=><p>{val.name} {val.exercises}</p>)}
+      <h4>total of {props.courses[1].parts.reduce(function(acc,obj){return acc+obj.exercises},0)} courses</h4>
    </div>
   )
 
@@ -19,32 +20,52 @@ const Course = (props) =>{
 
 const App = () => {
 
-  
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  let totalvalue=course.parts.reduce(function (acc, obj) { return acc + obj.exercises; }, 0)
-
-  return <Course course={course} value={totalvalue} />
+  return <Course courses={courses} />
 }
 
 export default App
