183366d81b0c019738318e3685cddfca269d1130
rajoy99 <rafat.joy99@gmail.com>
2022-01-21 16:03:10 +0600

    key added

--- part2/src/App.js
+++ part2/src/App.js
@@ -7,7 +7,7 @@ const App = (props) => {
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note=><li>{note.content}</li>)}
+        {notes.map(note=><li key={note.id}>{note.content}</li>)}
       </ul>
     </div>
   )
