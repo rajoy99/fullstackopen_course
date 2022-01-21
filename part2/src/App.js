e9fe3021b533911c262497dc21aa53fb3a7ea83f
rajoy99 <rafat.joy99@gmail.com>
2022-01-21 19:02:08 +0600

    2.1 done

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,16 +1,44 @@
 import React from 'react'
 
-const App = (props) => {
-  const { notes } = props
 
-  return (
-    <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map(note=><li key={note.id}>{note.content}</li>)}
-      </ul>
-    </div>
+
+const Course = (props) =>{
+
+
+  return(
+   <div>
+     <h1>{props.course.name}</h1>
+      {props.course.parts.map(val=><p>{val.name} {val.exercises}</p>)}
+   </div>
   )
+
+}
+
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
 }
 
 export default App
