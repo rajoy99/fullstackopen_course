80b82456f1a1e68583aac91e9fe5a70dd46c97c7
rajoy99 <rafat.joy99@gmail.com>
2022-01-22 00:04:58 +0600

    doing

--- part2/2.6to2.10/src/App.js
+++ part2/2.6to2.10/src/App.js
@@ -2,21 +2,31 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: '006900 '}
   ]) 
 
 
   const [newName, setNewName] = useState('')
+  const [newNumber,setNewNumber] = useState('')
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
+
+
   const addName = (event) => {
     event.preventDefault()
     const nameObject = {
       name: newName,
+      number: newNumber,
       date: new Date().toISOString(),
       
     }
@@ -44,6 +54,14 @@ const App = () => {
           value={newName}
           onChange={handleNameChange}/>
         </div>
+        <div>
+          number: <input
+          value={newNumber}
+          onChange={handleNumberChange}
+          />
+          
+          </div>
+
         <div>
           <button type="submit">add</button>
         </div>
