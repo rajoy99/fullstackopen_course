4f461b334bc3b1d5c230f41afe76991717949e81
rajoy99 <rafat.joy99@gmail.com>
2022-01-24 20:45:47 +0600

    axios fetching

--- part2/2.11to2.14/src/index.js
+++ part2/2.11to2.14/src/index.js
@@ -3,17 +3,10 @@ import App from './App.js'
 
 import axios from 'axios'
 
-const promise = axios.get('http://localhost:3001/notes')
-console.log(promise)
-
-const promise2 = axios.get('http://localhost:3001/foobar')
-console.log(promise2)
-
-promise.then(response => {
-  console.log(response)
+axios.get('http://localhost:3001/notes').then(response => {
+  const notes = response.data
+  ReactDOM.render(
+    <App notes={notes} />,
+    document.getElementById('root')
+  )
 })
-
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root')
-)
