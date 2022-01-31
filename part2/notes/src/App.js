72571e4b2e7ea51d55c89b1b6d3b09d288e85285
rajoy99 <rafat.joy99@gmail.com>
2022-01-31 13:49:06 +0600

    final version

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,23 +1,20 @@
 import React, { useState, useEffect } from 'react'
 import Note from './components/Note'
-import axios from 'axios'
 import noteService from './services/notes'
 
-
-
 const App = () => {
+
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(false)
 
   useEffect(() => {
     noteService
-    .getAll()
-    .then(response => {
-      setNotes(response.data)
+      .getAll()
+      .then(initialNotes => {
+      setNotes(initialNotes)
     })
   }, [])
-  console.log('render', notes.length, 'notes')
 
   const addNote = (event) => {
     event.preventDefault()
@@ -29,10 +26,27 @@ const App = () => {
 
     noteService
       .create(noteObject)
-      .then(response => {
-        setNotes(notes.concat(response.data))
+        .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
         setNewNote('')
       })
+  }
+
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+  
+    noteService
+    .update(id, changedNote)
+      .then(returnedNote => {
+      setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+    })
+    .catch(error => {
+      alert(
+        `the note '${note.content}' was already deleted from server`
+      )
+      setNotes(notes.filter(n => n.id !== id))
+    })    
   }
 
   const handleNoteChange = (event) => {
@@ -40,24 +54,9 @@ const App = () => {
     setNewNote(event.target.value)
   }
 
-  const toggleImportanceOf = id => {
-    const url = `http://localhost:3001/notes/${id}`
-    const note = notes.find(n => n.id === id)
-    const changedNote = { ...note, important: !note.important }
-  
-    noteService
-    .update(id, changedNote)
-    .then(response => {
-      setNotes(notes.map(note => note.id !== id ? note : response.data))
-    })
-
-  }
-
-
-
   const notesToShow = showAll
   ? notes
-  : notes.filter(note => note.important)
+  : notes.filter(note => note.important==true)
 
   return (
     <div>
@@ -66,17 +65,23 @@ const App = () => {
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all' }
         </button>
-      </div>      
+      </div>   
       <ul>
-        {notesToShow.map((note, i) => 
-          <Note
-            key={i}
-            note={note} 
-            toggleImportance={() => toggleImportanceOf(note.id)}
-          />
+        {notesToShow.map(note => 
+            <Note
+              key={note.id}
+              note={note} 
+              toggleImportance={() => toggleImportanceOf(note.id)}
+            />
         )}
       </ul>
-   
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>  
     </div>
   )
 }
--- part2/notes/src/services/notes.js
+++ part2/notes/src/services/notes.js
@@ -1,20 +1,32 @@
 import axios from 'axios'
-const baseUrl='http://localhost/3001/notes'
+const baseUrl = 'http://localhost:3001/notes'
 
-const getAll = () =>{
-    return axios.get(baseUrl)
+const getAll2 = () => {
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
 }
 
-const create = newobject =>{
-    return axios.post(baseUrl,newobject)
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  const nonExisting = {
+    id: 10000,
+    content: 'This note is not saved to server',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true,
+  }
+  return request.then(response => response.data.concat(nonExisting))
 }
 
-const update = (id,newobject) =>{
-    return axios.put(`${baseUrl}/${id}`,newobject)
+const create = newObject => {
+  const request = axios.post(baseUrl, newObject)
+  return request.then(response => response.data)
+}
+
+const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then(response => response.data)
 }
 
 export default { 
-    getAll: getAll, 
-    create: create, 
-    update: update 
-  }
+  getAll, create, update
+}
