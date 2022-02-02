68ff17c5d781a4c9bd621e19d60dbda190a588e0
rajoy99 <rafat.joy99@gmail.com>
2022-02-02 16:56:46 +0600

    fetching

--- part3/index.js
+++ part3/index.js
@@ -24,12 +24,11 @@ let notes = [
   }
 ]
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
 
-app.get('/api/notes', (request, response) => {
-  response.json(notes)
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+  response.json(note)
 })
 
 const PORT = 3001
